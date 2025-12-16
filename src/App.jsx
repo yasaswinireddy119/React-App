b7762389cb80d0faeba1a26999ecd22b7fb44a69
yasaswinireddy119 <yasaswinireddykotha@gmail.com>
2025-12-16 21:44:02 +0530

    Created first react component

---
+++ src/App.jsx
@@ -0,0 +1,9 @@
+import FirstComponent from "./FirstComponent";
+function App(){
+  return(
+    <div>
+      <FirstComponent/>
+    </div>
+  )
+}
+export default App;
---
+++ src/FirstComponent.jsx
@@ -0,0 +1,6 @@
+function FirstComponent(){
+    return(
+        <h2>This is my first React Component</h2>
+    )
+}
+export default FirstComponent
