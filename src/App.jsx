cf54b58419ca4f5fa5db80615a3c528d85820ffe
yasaswinireddy119 <yasaswinireddykotha@gmail.com>
2025-12-17 21:34:48 +0530

    messagecard

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,28 @@
-import Counter from "./Counter";
+import MessageCard from "./assignment4/messageCard";
 
 function App() {
   return (
-    <Counter />
+    <div style={{ display: "flex", flexWrap: "wrap" }}>
+      <MessageCard
+        title="Welcome"
+        message="This is my first message card."
+      />
+
+      <MessageCard
+        title="React Props"
+        message="Props allow data to be passed to components."
+      />
+
+      <MessageCard
+        title="Reusable Component"
+        message="Same component, different content."
+      />
+
+      <MessageCard
+        title="Success"
+        message="You have rendered multiple cards!"
+      />
+    </div>
   );
 }
 
---
+++ src/assignment4/messageCard.jsx
@@ -0,0 +1,18 @@
+function MessageCard({ title, message }) {
+  return (
+    <div
+      style={{
+        border: "1px solid #ccc",
+        padding: "15px",
+        margin: "10px",
+        borderRadius: "8px",
+        width: "250px"
+      }}
+    >
+      <h3>{title}</h3>
+      <p>{message}</p>
+    </div>
+  );
+}
+
+export default MessageCard;
